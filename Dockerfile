3803f954749384169c22508e7bec7b3d80c7e9c3
caviri <carlosvivarrios@gmail.com>
2023-08-17 09:05:49 +0200

    Updated dockerfile and entry page.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.10
-RUN pip install streamlit streamlit-aggrid
-RUN pip install st_pages
+RUN pip install streamlit streamlit-aggrid 
+RUN pip install st_pages barfi boto3 pymongodb
 #RUN pip install git+https://github.com/NeveIsa/streamlit_ttyd
 
 COPY ./odtp /odtp
--- odtp/gui/app.py
+++ odtp/gui/app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 
-st.write("ODTP dev platform")
-st.markdown("List Components")
+st.write("# ODTP dev platform")
+st.markdown("""
+            This is a GUI to support the development of ODTP. 
+            It can:
 
-st.sidebar.image("img/logo.png", width=300)
+            - Restart the folder structure in S3
+            - Restart the schema in MONGODB
+            - Visualize logs
+            - Visualize and download files in S3
+            - Visualize datasets in output.zip
+            """)
+
+#st.sidebar.image("img/logo.png", width=300)
 from st_pages import show_pages_from_config
 
 show_pages_from_config()
