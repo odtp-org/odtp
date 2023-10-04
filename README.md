15d7e8f22aa59ec4c30720d7437e19986fe55670
caviri <carlosvivarrios@gmail.com>
2023-10-04 17:35:21 +0200

    New schema for mongoDB

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ This tools allows you to:
 - Restart and test different schemas for MongoDB / S3.
 - Check outputs/snapshots and download results. 
 
-## Roadmap
+## Changelog
+
+- v.0.2.0: Improvements in database and files management.
+    - New MongoDB Schema. It will be apply after restarting the server. 
 
 
 - v.0.1.0: Basic UI
@@ -18,11 +21,11 @@ This tools allows you to:
     - User tagging
     - Component listing placeholder
     - Digital Twins listing
-    - Snapshots listing
-    - Workflow desginer
-    - pygwalker data visualization
-    - MongoDB is required to be deployed independtly
-    - S3 is required to be deployed indepently 
+    - Snapshots listing.
+    - Workflow desginer.
+    - pygwalker data visualization.
+    - MongoDB is required to be deployed independtly.
+    - S3 is required to be deployed indepently.
 
 ## Technologies involved
 
@@ -34,7 +37,6 @@ This tools allows you to:
 - pygwalker (Data Visualization)
 
 ## How to install ODTP
-
 ### How to install ODTP in Conda
 
 ```
@@ -60,7 +62,84 @@ docker build -t caviri/odtp .
 docker run -it --rm -p 8501:8501 caviri/odtp
 ```
 
+## Documentation 
 
-## Development 
+### How to deploy a mongoDB
+
+```
+docker run --name mongodb-instance -it -v /home/[USER]/mongodb:/data/db -e MONGO_INITDB_ROOT_USERNAME=[USER] -e MONGO_INITDB_ROOT_PASSWORD=[PASS] -e MONGO_INITDB_DATABASE=odtp -p 27017:27017 mongo:latest
+```
+
+### How to request an EPFL S3 Instance
+
+TOBEDONE
+
+### MongoDB Schema
+
+This is the schema for odtp database. 
+
+v.0.1.0 Schema
+
+```python
+# Users Collection
+users = {
+    "_id": ObjectId(),   # MongoDB will automatically create an _id
+    "displayName": "John Doe",
+    "email": "john@example.com",
+    "github": "johnDoeRepo"
+}
+
+# Components Collection
+components = {
+    "_id": ObjectId(),
+    "author": ObjectId(),  # Reference to Users collection
+    "componentName": "ComponentX"
+}
+
+# Versions Collection
+versions = {
+    "_id": ObjectId(),
+    "componentId": ObjectId(),  # Reference to Components collection
+    "version": "v1.0",
+    "repoLink": "https://github.com/...",
+    "dockerHubLink": "https://hub.docker.com/...",
+    "parameters": {...}   # Parameters JSON object
+}
+
+# DigitalTwins Collection
+digitalTwins = {
+    "_id": ObjectId(),
+    "userRef": ObjectId(),  # Reference to Users collection
+    "workflowSchema": {
+        "version": "v2.0",
+        "components": [ObjectId()],  # Array of ObjectIds referring to Components
+        "barfiSchema": {...},  # Barfi Schema JSON object
+        "public": True
+    },
+    "executions": [{
+        "timestamp": "2023-10-03T10:15:30Z",
+        "steps": [{
+            "timestamp": "2023-10-03T10:16:00Z",
+            "logs": "...",
+            "inputs": {...},
+            "outputs": {...},
+            "component": ObjectId(),  # Reference to Components collection
+            "component_version": "v1.0",
+            "parameters": {...}, 
+            "snapshot": "..."
+        }]
+    }]
+}
+
+# Results Collection
+results = {
+    "_id": ObjectId(),
+    "executionRef": ObjectId(),  # Reference to DigitalTwins.execution
+    "outputs": {...}
+}
+```
+
+
+## Development.
 
 Developed by SDSC/CSFM
--- odtp/gui/init.py
+++ odtp/gui/init.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from bson import ObjectId
 import boto3
 import ast
+import json
 
 st.set_page_config(
     page_title="ODPT",
@@ -24,6 +26,254 @@ col1, col2 = st.columns(2)
 # This is to provide an easy configuration and reseting of MONGODB and the S3 Enviroment
 
 #################################################################################
+# Testing Class MongoManager with v.0.2.0 Schema version of ID.
+
+class MongoManager:
+    def __init__(self, mongodbUrl,db_name):
+        self.client = MongoClient(mongodbUrl)
+        self.db = self.client[db_name]
+
+    def add_user(self, displayName, email, github):
+        users_collection = self.db["users"]
+        user_data = {
+            "displayName": displayName,
+            "email": email,
+            "github": github
+        }
+        return users_collection.insert_one(user_data).inserted_id
+
+    def add_component(self, author_id, componentName):
+        components_collection = self.db["components"]
+        component_data = {
+            "author": author_id,
+            "componentName": componentName
+        }
+        return components_collection.insert_one(component_data).inserted_id
+
+    def add_version(self, componentId, version, repoLink, dockerHubLink, parameters={}):
+        versions_collection = self.db["versions"]
+        version_data = {
+            "componentId": componentId,
+            "version": version,
+            "repoLink": repoLink,
+            "dockerHubLink": dockerHubLink,
+            "parameters": parameters
+        }
+        return versions_collection.insert_one(version_data).inserted_id
+
+    def add_digital_twin(self, userRef, version, components, barfiSchema={}, public=True, execution=[]):
+        digital_twins_collection = self.db["digitalTwins"]
+        digital_twin_data = {
+            "userRef": userRef,
+            "workflowSchema": {
+                "version": version,
+                "components": components,
+                "barfiSchema": barfiSchema,
+                "public": public
+            },
+            "executions": execution
+        }
+        return digital_twins_collection.insert_one(digital_twin_data).inserted_id
+
+    def add_result(self, execution_ref, execution_timestamp, outputs):
+        """
+        Add a result entry to the results collection.
+
+        :param execution_ref: Reference to an execution in DigitalTwins.execution.
+        :param outputs: Outputs associated with the result.
+        :return: The ObjectId of the created result document.
+        """
+        results_collection = self.db["results"]
+        result = {
+            "executionRef": execution_ref,
+            "execution.timestamp": execution_timestamp,
+            "outputs": outputs
+        }
+        return results_collection.insert_one(result).inserted_id
+    
+    def append_execution(self, digital_twin_id, execution_data):
+        """
+        Append a new execution to a specific digital twin.
+
+        :param digital_twin_id: ObjectId of the digital twin.
+        :param execution_data: The execution data to append.
+        """
+        digital_twins_collection = self.db["digitalTwins"]
+        digital_twins_collection.update_one(
+            {"_id": digital_twin_id},
+            {"$push": {"executions": execution_data}}
+        )
+
+    def append_step(self, digital_twin_id, execution_timestamp, step_data):
+        """
+        Append a step to a specific execution within a digital twin.
+
+        :param digital_twin_id: ObjectId of the digital twin.
+        :param execution_timestamp: Timestamp of the specific execution.
+        :param step_data: The step data to append.
+        """
+        digital_twins_collection = self.db["digitalTwins"]
+        digital_twins_collection.update_one(
+            {"_id": digital_twin_id, "execution.timestamp": execution_timestamp},
+            {"$push": {"executions.$.steps": step_data}}
+        )
+
+    def append_log(self, digital_twin_id, execution_timestamp, step_timestamp, log_entry):
+        """
+        Append a log entry to a specific step of a specific execution within a digital twin.
+
+        :param digital_twin_id: ObjectId of the digital twin.
+        :param execution_timestamp: Timestamp of the specific execution.
+        :param step_timestamp: Timestamp of the specific step.
+        :param log_entry: The log entry (dictionary) to append.
+        """
+        digital_twins_collection = self.db["digitalTwins"]
+        
+        # Get the document with the specific digital twin ID
+        doc = digital_twins_collection.find_one({"_id": digital_twin_id})
+        
+        # Find the index of the specific execution and step
+        execution_index = -1
+        step_index = -1
+        for i, execution in enumerate(doc["executions"]):
+            if execution["timestamp"] == execution_timestamp:
+                execution_index = i
+                for j, step in enumerate(execution["steps"]):
+                    if step["timestamp"] == step_timestamp:
+                        step_index = j
+                        break
+                break
+
+        # If the execution and step are found, update the logs for that step
+        if execution_index != -1 and step_index != -1:
+            update_path = f"executions.{execution_index}.steps.{step_index}.logs"
+            digital_twins_collection.update_one(
+                {"_id": digital_twin_id},
+                {"$push": {update_path: log_entry}}
+            )
+
+    def get_all_collections_as_dict(self):
+            """
+            Retrieve all documents in all collections as a dictionary.
+            """
+            all_data = {}
+            for collection_name in self.db.list_collection_names():
+                cursor = self.db[collection_name].find()
+                all_data[collection_name] = [doc for doc in cursor]
+            
+            return all_data
+
+    def get_all_collections_as_json_string(self):
+        """
+        Retrieve all documents in all collections as a JSON-formatted string.
+        """
+        all_data = self.get_all_collections_as_dict()
+        return json.dumps(all_data, indent=2, default=str)  # default=str to handle datetime and ObjectId
+
+
+    def print_all_collections_as_json(self):
+        """
+        Print all documents in all collections in JSON format.
+        """
+        for collection_name in self.db.list_collection_names():
+            print(f"Collection: {collection_name}")
+            cursor = self.db[collection_name].find()
+            for doc in cursor:
+                print(json.dumps(doc, indent=2, default=str))  # default=str is added to handle datetime and ObjectId
+            print("-" * 50)  # separator line between collections
+
+    def close(self):
+        self.client.close()
+
+
+##################################################################################
+# Testing the DB with mockup data
+
+from datetime import datetime, timedelta
+
+from random import choice
+
+class MockDataEmulator:
+    def __init__(self, db_manager):
+        self.db_manager = db_manager
+
+    def emulate_data(self):
+        # Emulating some users
+        user_ids = [
+            self.db_manager.add_user("John Doe", "john@example.com", "johnDoeRepo"),
+            self.db_manager.add_user("Alice Smith", "alice@example.com", "aliceSmithRepo"),
+            self.db_manager.add_user("Bob Johnson", "bob@example.com", "bobJohnsonRepo")
+        ]
+
+        # Emulating some components for each user
+        component_ids = [
+            self.db_manager.add_component(user_id, f"Component_{i}")
+            for i, user_id in enumerate(user_ids, 1)
+        ]
+
+        # Emulating versions for each component
+        version_links = [
+            ("https://github.com/John/Component_1", "https://hub.docker.com/John/Component_1"),
+            ("https://github.com/Alice/Component_2", "https://hub.docker.com/Alice/Component_2"),
+            ("https://github.com/Bob/Component_3", "https://hub.docker.com/Bob/Component_3")
+        ]
+
+        for i, component_id in enumerate(component_ids):
+            self.db_manager.add_version(
+                component_id,
+                f"v{i+1}.0",
+                version_links[i][0],
+                version_links[i][1]
+            )
+
+        # Emulating a digital twin for a user
+        dt_id = self.db_manager.add_digital_twin(
+            choice(user_ids),
+            "v1.0",
+            component_ids
+        )
+
+        execution_timestamp = datetime.now().isoformat()
+        step_timestamp = (datetime.now() + timedelta(minutes=1)).isoformat()  # for demonstration
+
+        step_data = {
+            "timestamp": step_timestamp,
+            "logs": [],  # start with empty logs
+            "inputs": {"input1": "data1"},
+            "outputs": {"output1": "result1"},
+            "component": choice(component_ids),
+            "component_version": "v1.0",
+            "parameters": {"param1": "value1"},
+            "snapshot": "Snapshot data..."
+        }
+
+        execution_data = {
+            "timestamp": execution_timestamp,
+            "steps": [step_data]
+        }
+        
+        self.db_manager.append_execution(dt_id, execution_data)
+
+        # Using the append_log method to add logs to the above step
+        for _ in range(3):  # adding 3 logs for demonstration purposes
+            log_entry = {
+                "timestamp": datetime.now().isoformat(),
+                "logstring": f"Emulated log entry {_+1}..."
+            }
+            self.db_manager.append_log(dt_id, execution_timestamp, step_timestamp, log_entry)
+
+        # Emulating results for the executed digital twin
+        outputs = {
+            "finalOutput1": "finalResult1",
+            "finalOutput2": "finalResult2"
+        }
+        # Here, we'll use the `dt_id` as the execution reference for demonstration purposes
+        # In a real scenario, the executionRef would point to a specific execution within a digital twin
+        self.db_manager.add_result(dt_id, execution_timestamp, outputs)
+
+
+##################################################################################
+
 
 def createCollections(mongoString, collectionsToCreate):
     # Connect to the MongoDB server. Replace 'localhost' with your server address and 27017 with your port if different.
@@ -73,17 +323,18 @@ def checkCollections(mongoString):
     collection_names = db.list_collection_names()
     st.write(collection_names)
 
-    return collection_names
-
     # Close the connection.
     client.close()
+
+    return collection_names
 
 with col1: 
     st.markdown("## MongoDB")
 
     mongoString = st.text_input("MongoString", value="mongodb://USER:PASS@10.95.48.38:27017/")
-    collectionsText = st.text_input("Introduce collections to create", value=['logs', 'users', 'components', 'snapshots', 'digitalTwins'])
+    collectionsText = st.text_input("Introduce collections to create", value=['users', 'components','versions','digitalTwins', 'results'])
     create = st.button("Create Collections on MongoDB")
+    mockupData = st.button("Create mock up data")
     delete = st.button("Delete Collections on MongoDB")
     check = st.button("Check Collections on MongoDB")
 
@@ -95,10 +346,20 @@ with col1:
         deleteCollections(mongoString)
         st.write("Collections Deleted")
 
+    if mockupData:
+        db_manager = MongoManager(mongoString, "odtp")
+        emulator = MockDataEmulator(db_manager)
+        emulator.emulate_data()
+        db_manager.close()
+
     if check:
-        coll = checkCollections(mongoString)
-        st.write("Collections Check")
-        st.json(coll)
+        # coll = checkCollections(mongoString)
+        # st.write("Collections Check")
+        # st.json(coll)
+        db_manager = MongoManager(mongoString, "odtp")
+        all_data_string = db_manager.get_all_collections_as_json_string()
+        db_manager.close()
+        st.json(all_data_string)
 
 
 ####################################################
