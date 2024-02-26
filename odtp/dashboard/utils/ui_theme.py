ba377f1c5562b88f869d329bc4d5f4e139e06a15
{sabine.maennel} <{sabine.maennel@gmail.com}>
2024-02-26 19:49:18 +0100

    choire(dashboard/ui_theme): remove unneeded prepare page
    no extra page is needed for preparing executions: this will be
    all done from the run page
    
    add ui element for items that are missing for a page to work, such
    as digital twins on the execution page (the user needs to select
    a digital twin first)

--- odtp/dashboard/utils/ui_theme.py
+++ odtp/dashboard/utils/ui_theme.py
@@ -3,16 +3,19 @@ from contextlib import contextmanager
 
 from nicegui import app, ui
 
+import odtp.dashboard.utils.helpers as helpers
 import odtp.dashboard.utils.storage as storage
+import odtp.helpers.utils as odtp_utils
 
 PATH_ABOUT = "/"
 PATH_USERS = "/users"
 PATH_DIGITAL_TWINS = "/digital-twins"
 PATH_COMPONENTS = "/components"
 PATH_EXECUTIONS = "/executions"
-PATH_PREPARE = "/prepare"
 PATH_RUN = "/run"
 
+NO_SELECTION_VALUE = "None"
+NO_SELECTION_INPUT = None
 
 def menu() -> None:
     ui.link("About", PATH_ABOUT).classes(replace="text-white")
@@ -20,7 +23,6 @@ def menu() -> None:
     ui.link("Digital Twins", PATH_DIGITAL_TWINS).classes(replace="text-white")
     ui.link("Components", PATH_COMPONENTS).classes(replace="text-white")
     ui.link("Executions", PATH_EXECUTIONS).classes(replace="text-white")
-    ui.link("Prepare", PATH_PREPARE).classes(replace="text-white")
     ui.link("Run", PATH_RUN).classes(replace="text-white")
 
 
@@ -35,3 +37,16 @@ def frame(navtitle: str):
         with ui.row():
             menu()
     yield
+
+
+def ui_add_first(item_name, page_link):
+        ui.label(f"You need to select a {item_name} first").classes('text-lg')
+        ui.button(
+            f"Select {item_name}",
+            on_click=lambda: ui.open(page_link),
+            icon="link",
+        )
+
+
+def ui_no_items_yet(item_name_plural):
+    ui.label(f"There are no {item_name_plural} yet. Start adding {item_name_plural}").classes('text-lg')
