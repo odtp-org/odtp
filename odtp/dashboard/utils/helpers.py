1b4f027c8a41b7961d890e0ab0b6f27841d75db1
{sabine.maennel} <{sabine.maennel@gmail.com}>
2024-02-26 20:58:20 +0100

    feature(dashboard/helpers.py): add helper to display mermaid
    add helper to display executions as mairmaid diagrams
    
    remove helper for execution prepare and run as only executions
    may be prepared and run via the gui

--- odtp/dashboard/utils/helpers.py
+++ odtp/dashboard/utils/helpers.py
@@ -1,44 +1,10 @@
-import os
-import re
-
-ODTP_INPUT = "odtp-input"
-ODTP_OUTPUT = "odtp-output"
-ODTP_REPO = "repository"
-
-
-def check_empty_output_folder(project_path):
-    if not os.path.exists(project_path):
-        return False
-    elif not os.path.isdir(project_path):
-        return False
-    elif len(os.listdir(project_path)) != 0:
-        return False
-    return True
-
-
-def check_output_folder(project_path):
-    if not os.path.exists(project_path):
-        return False
-    elif not os.path.isdir(project_path):
-        return False
-    return True
-
-
-def map_output_folder(project_path):
-    project_content = {}
-    output_path = os.path.join(project_path, ODTP_OUTPUT)
-    input_path = os.path.join(project_path, ODTP_INPUT)
-    if os.path.exists(output_path):
-        project_content[ODTP_OUTPUT] = os.listdir(output_path)
-    if os.path.exists(input_path):
-        project_content[ODTP_INPUT] = os.listdir(input_path)
-    print(project_content)
-    return project_content
-
-
-def check_docker_image_name(value):
-    if not len(value) >= 4:
-        return False
-    if not re.match("^[a-z]+$", value):
-        return False
-    return True
+def get_workflow_mairmaid(step_names):
+    if len(step_names) == 1:
+        return step_names[0]
+    step_name_tuples = [
+        (step_names[i - 1], step_names[i]) for i in range(len(step_names)) if i > 0
+    ]
+    workflow_in_mermaid = ""
+    for step_tuple in step_name_tuples:
+        workflow_in_mermaid += f"{step_tuple[0]} --> {step_tuple[1]};"
+    return workflow_in_mermaid
