9b48567d5c0cfdc96244552c71609437fc7ca79d
{sabine.maennel} <{sabine.maennel@gmail.com}>
2024-02-26 17:52:22 +0100

    feat(helpers): add helpers for git and for execution step names
    these helpers can be used from both the gui and from the command
    line interface
    
    the git helpers check github urls and bring back information on
    repos from the github api, that will be displayed in the
    dashboard
    
    helpers to derive version names for executions are also provided
    these version names are formed of the component version and the
    component name and are used to reference the component versions
    in the file output struture as well as in the gui. Version ids
    are used only in the command line user interface and internally,
    but are not displayed in the gui

--- odtp/helpers/environment.py
+++ odtp/helpers/environment.py
@@ -1,12 +1,58 @@
 import os
 
+import odtp.helpers.utils as utils
+import odtp.helpers.utils as odtp_utils
+import odtp.mongodb.db as db
+
 
 class OdtpLocalEnvironmentException(Exception):
     pass
 
 
-def check_project_folder_empty(project_folder):    
-    """check whether the project folder is an empty folder"""
+def check_project_folder_empty(project_folder):
+    if not project_folder_is_empty(project_folder):
+        raise OdtpLocalEnvironmentException(
+            f"project folder {project_folder} was not empty"
+        )
+
+
+def check_directory_folder_matches_execution(execution_id, project_folder):
+    if not directory_folder_matches_execution(execution_id, project_folder):
+        raise OdtpLocalEnvironmentException(
+            f"""project folder {project_folder} does not match execution. 
+            First run 'prepare' on an empty folder to prepare for the run of the execution"""
+        )
+
+
+def project_folder_is_empty(project_folder):
     empty_directory = os.path.isdir(project_folder) and not os.listdir(project_folder)
     if not empty_directory:
-        raise OdtpLocalEnvironmentException(f"project folder {project_folder} was not empty")
+        return False
+    return True
+
+
+def directory_folder_matches_execution(execution_id, project_folder):
+    if not os.path.isdir(project_folder):
+        return False
+    subdirs = os.listdir(project_folder)
+    execution = db.get_document_by_id(
+        document_id=execution_id, collection=db.collection_executions
+    )
+    step_names = odtp_utils.get_version_names_for_execution(execution)
+    expected_dir_names = [f"{i}_{step}" for i, step in enumerate(step_names)]
+    if set(subdirs) != set(expected_dir_names):
+        return False
+    return True
+
+
+def directory_has_output(execution_id, project_folder):
+    if not directory_folder_matches_execution(execution_id, project_folder):
+        return False
+    subdirs = os.listdir(project_folder)
+    output_dirs = [
+        os.path.join(project_folder, subdir, "odtp-output") for subdir in subdirs
+    ]
+    for output_dir in output_dirs:
+        if len(os.listdir(output_dir)) != 0:
+            return True
+    return False
--- odtp/helpers/utils.py
+++ odtp/helpers/utils.py
@@ -1,9 +1,11 @@
 import json
+
+import odtp.mongodb.db as db
 from odtp import __version__
 
 
 def get_odtp_version():
-    return(__version__)
+    return __version__
 
 
 def output_as_json(db_output):
@@ -15,9 +17,31 @@ def print_output_as_json(db_output):
 
 
 def get_workflow(versions):
-    return(list(range(len(versions))))
+    return list(range(len(versions)))
+
 
 def get_execution_step_name(component_name, component_version, step_index=None):
     if step_index is None:
         return f"{component_name}_{component_version}"
     return f"{step_index}_{component_name}_{component_version}"
+
+
+def get_version_name_dict_for_version_ids(version_ids):
+    versions = db.get_document_by_ids_in_collection(
+        collection=db.collection_versions, document_ids=version_ids
+    )
+    versions_dict = {}
+    for version in versions:
+        version_display_name = get_execution_step_name(
+            component_name=version["component"]["componentName"],
+            component_version=version["component_version"],
+        )
+        versions_dict[str(version["_id"])] = version_display_name
+    return versions_dict
+
+
+def get_version_names_for_execution(execution):
+    version_ids = execution["workflowSchema"]["component_versions"]
+    version_dict = get_version_name_dict_for_version_ids(version_ids)
+    version_names = [version_dict[version_id] for version_id in version_ids]
+    return version_names
