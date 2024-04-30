399f0c3e756d4024da6f18d31119275c7053fd41
sabinem <{sabine.maennel@gmail.com}>
2024-04-30 07:52:46 +0200

    feat(dashboard helpers): improve mermaid and add new helpers
    display also same components as different steps in the workflow
    
    initialize mermaid diagram since this is necessary even if it does
    not carry any items
    
    add new functions to get keys and values form parameters
    
    add custom dashboard function for display of component versions

--- odtp/dashboard/utils/helpers.py
+++ odtp/dashboard/utils/helpers.py
@@ -1,13 +1,15 @@
-def get_workflow_mairmaid(step_names):
-    if len(step_names) == 1:
-        return step_names[0]
-    step_name_tuples = [
-        (step_names[i - 1], step_names[i]) for i in range(len(step_names)) if i > 0
-    ]
-    workflow_in_mermaid = ""
-    for step_tuple in step_name_tuples:
-        workflow_in_mermaid += f"{step_tuple[0]} --> {step_tuple[1]};"
-    return workflow_in_mermaid
+def get_workflow_mermaid(step_names, init="graph TB;"):
+    mermaid_graph = init
+    step_count = len(step_names)
+    if step_count == 1:
+        mermaid_graph += f"{step_names[0]};"
+    elif step_count > 1:    
+        step_name_tuples = [
+            (step_names[i - 1], step_names[i]) for i in range(step_count) if i > 0
+        ]
+        for i, step_tuple in enumerate(step_name_tuples):
+            mermaid_graph += f"C{i}[{step_tuple[0]}] --> C{i+1}[{step_tuple[1]}];"
+    return mermaid_graph
 
 
 def pd_lists_to_counts(items_list):
@@ -26,3 +28,22 @@ def component_version_for_table(version):
         "commit": version.get("commitHash")[:8]
     }
     return version_cleaned
+
+
+def get_execution_step_display_name(component_name, component_version):
+    display_name = f"{component_name}:{component_version}"
+    return display_name    
+
+
+def get_key_from_parameters(current_component_parameters, index):
+    if index >= len(current_component_parameters):
+        return ""
+    parameters_keys_as_list = list(current_component_parameters.keys())  
+    return parameters_keys_as_list[index]
+
+
+def get_value_from_parameters(current_component_parameters, index):
+    if index >= len(current_component_parameters):
+        return ""    
+    parameters_values_as_list = list(current_component_parameters.values())
+    return parameters_values_as_list[index]           
