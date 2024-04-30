7c2dd190eef735d9902ca152a2daefee808fda38
sabinem <5292683+sabinem@users.noreply.github.com>
2024-04-30 07:09:51 +0200

    feat(validators for execution form): add new validators
    add validator for step number to be an integer  below a threshold
    
    add validator for a port mapping

--- odtp/dashboard/utils/validators.py
+++ odtp/dashboard/utils/validators.py
@@ -20,6 +20,14 @@ def validate_required_input(value):
     return True
 
 
+def validate_integer_input_below_threshold(value, lower_bound, upper_bound):
+    if not int(value) == value:
+        return False
+    if not lower_bound <= value <= upper_bound:
+        return False    
+    return True        
+
+
 def validate_github_url(value):
     try:
         repo_info = otdp_git.get_github_repo_info(value)
@@ -28,3 +36,13 @@ def validate_github_url(value):
     except Exception as e:
         raise(e)
     return True
+
+
+def validate_ports_mapping_input(value):
+    if not value:
+        return True
+    try:
+        if re.match(db_utils.PORT_MAPPING_PATTERN, value):
+            return True
+    except Exception as e:
+        return False
