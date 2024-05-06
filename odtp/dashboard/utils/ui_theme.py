f0c09ee9884ff35229989347d522dc2279cde628
sabinem <5292683+sabinem@users.noreply.github.com>
2024-05-06 15:01:30 +0200

    feat(dashboard menu): change sequence of pages in the menu
    digital twins and executions should be next to each other

--- odtp/dashboard/utils/ui_theme.py
+++ odtp/dashboard/utils/ui_theme.py
@@ -22,8 +22,8 @@ NO_SELECTION_INPUT = None
 def menu() -> None:
     ui.link("About", PATH_ABOUT).classes(replace="text-white")
     ui.link("Users", PATH_USERS).classes(replace="text-white")
-    ui.link("Digital Twins", PATH_DIGITAL_TWINS).classes(replace="text-white")
     ui.link("Components", PATH_COMPONENTS).classes(replace="text-white")
+    ui.link("Digital Twins", PATH_DIGITAL_TWINS).classes(replace="text-white")
     ui.link("Executions", PATH_EXECUTIONS).classes(replace="text-white")
     ui.link("Run", PATH_RUN).classes(replace="text-white")
 
