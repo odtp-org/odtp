5f438a5d49da81aa96dba8e1a1681e096b8acbe5
sabinem <5292683+sabinem@users.noreply.github.com>
2024-05-06 23:31:35 +0200

    fix(version upgrade): upgrade version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odtp',
-    version='0.2.2',
+    version='0.2.5',
     packages=find_packages(),
     description='Open Digital Twin Platform',
     long_description=open('README.md').read(),
