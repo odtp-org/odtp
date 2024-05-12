f6aa64d4245d43c888024878653850256132463c
sabinem <5292683+sabinem@users.noreply.github.com>
2024-05-12 11:22:51 +0200

    fix(settings): eval needs strings not booleans

--- odtp/helpers/settings.py
+++ odtp/helpers/settings.py
@@ -19,9 +19,9 @@ try:
     ODTP_ACCESS_KEY = os.getenv("ODTP_ACCESS_KEY")
     ODTP_SECRET_KEY = os.getenv("ODTP_SECRET_KEY")
     ODTP_DASHBOARD_PORT = int(os.getenv("ODTP_DASHBOARD_PORT"))
-    ODTP_DASHBOARD_RELOAD = eval(os.getenv("ODTP_DASHBOARD_RELOAD", False))
+    ODTP_DASHBOARD_RELOAD = eval(os.getenv("ODTP_DASHBOARD_RELOAD", "False"))
     ODTP_LOG_LEVEL=os.getenv("INTERNAL_LOG_LEVEL", "INFO")
-    ODTP_DASHBOARD_JSON_EDITOR = eval(os.getenv("INTERNAL_JSON_EDITOR", False))
+    ODTP_DASHBOARD_JSON_EDITOR = eval(os.getenv("ODTP_DASHBOARD_JSON_EDITOR", "False"))
     ODTP_PATH = os.getenv("ODTP_PATH")
 except Exception as e:
     raise OdtpSettingsException(f"Configuration of ODTP raised an exception {e}")
