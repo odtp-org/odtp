d4c3b6afb2f160f245a36a0c5932ba0afd8c9540
sabrinaossey <sabrina.ossey@epfl.ch>
2024-05-16 10:55:10 +0200

    feat(utils): Add option for displaying authentication page

--- odtp/dashboard/utils/ui_theme.py
+++ odtp/dashboard/utils/ui_theme.py
@@ -7,6 +7,7 @@ import odtp.dashboard.utils.helpers as helpers
 import odtp.dashboard.utils.storage as storage
 import odtp.helpers.utils as odtp_utils
 import odtp.mongodb.db as db
+import odtp.helpers.settings as config
 
 
 PATH_ABOUT = "/"
@@ -15,19 +16,23 @@ PATH_DIGITAL_TWINS = "/digital-twins"
 PATH_COMPONENTS = "/components"
 PATH_EXECUTIONS = "/executions"
 PATH_RUN = "/run"
+PATH_SIGN = "/logout"
 
 NO_SELECTION_VALUE = "None"
 NO_SELECTION_INPUT = None
 
 def menu() -> None:
     ui.link("About", PATH_ABOUT).classes(replace="text-white")
-    ui.link("Users", PATH_USERS).classes(replace="text-white")
     ui.link("Components", PATH_COMPONENTS).classes(replace="text-white")
     ui.link("Digital Twins", PATH_DIGITAL_TWINS).classes(replace="text-white")
     ui.link("Executions", PATH_EXECUTIONS).classes(replace="text-white")
     ui.link("Run", PATH_RUN).classes(replace="text-white")
-
-
+    if config.ODTP_AUTHENTICATION == True:
+        ui.button('user', on_click=lambda: (app.storage.user.clear(), ui.navigate.to(PATH_SIGN)),icon='logout')
+    else:
+        ui.link("Users", PATH_USERS).classes(replace="text-white")
+    
+        
 @contextmanager
 def frame(navtitle: str):
     """Custom page frame to share the same styling and behavior across all pages"""
