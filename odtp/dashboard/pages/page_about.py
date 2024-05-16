e68cc7cdcee678ce7ea136d58a11f402d48fe901
sabinem <5292683+sabinem@users.noreply.github.com>
2024-05-16 14:19:58 +0200

    style(dashboard page_about.py) improve about page
    align in  a reliable way with markdown
    
    add more links on the homepage

--- odtp/dashboard/pages/page_about.py
+++ odtp/dashboard/pages/page_about.py
@@ -6,8 +6,15 @@ def content() -> None:
             ui.markdown(
                 """
                         # OTDP (Open Digital Twin Project)
-                        
-                        ## How to get started:
+
+                        [Check out our Documentation](https://odtp-org.github.io/odtp-manuals/)
+
+                        ## Getting started:
+
+                        - [Add and select a user](/users)
+                        - [Add and select a digital twin](/digital-twins)
+                        - [Check that components for your workflow are there](/components)
+                        - [Setup an execution](/executions)
+                        - [Run the execution](/run)
                         """
-            )
-            ui.link("Check out our Documentation", "https://odtp-org.github.io/odtp-manuals/").classes('text-lg bg-100-teal')
+            ).style('font-size: 150%')
