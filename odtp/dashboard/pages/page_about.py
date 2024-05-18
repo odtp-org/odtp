22df96e636c20091cd5f11c9d216602b987c1490
sabinem <5292683+sabinem@users.noreply.github.com>
2024-05-18 11:01:56 +0200

    style(dashbaord page_about.py): prettify page

--- odtp/dashboard/pages/page_about.py
+++ odtp/dashboard/pages/page_about.py
@@ -2,19 +2,73 @@ from nicegui import ui
 
 
 def content() -> None:
-    with ui.row().classes('fixed-center'):
+    with ui.row():
+        ui.markdown(
+            """
+            # OTDP (Open Digital Twin Project)
+
+            ## Getting Started
+            """
+        )
+    with ui.row():        
+        ui.button(
+            "Check out the Documentation",
+            on_click=lambda: ui.open("https://odtp-org.github.io/odtp-manuals/"),
+            icon="link",
+        ).props("flat").classes("place-items-start")
+    with ui.grid(columns=2).classes("w-full"):
+        with ui.column().classes("w-full"):
             ui.markdown(
                 """
-                        # OTDP (Open Digital Twin Project)
-
-                        [Check out our Documentation](https://odtp-org.github.io/odtp-manuals/)
-
-                        ## Getting started:
-
-                        - [Add and select a user](/users)
-                        - [Add and select a digital twin](/digital-twins)
-                        - [Check that components for your workflow are there](/components)
-                        - [Setup an execution](/executions)
-                        - [Run the execution](/run)
-                        """
-            ).style('font-size: 150%')
+                ### Digital Twins
+                """
+            ).classes("w-full")
+            with ui.grid(columns=1):
+                ui.button(
+                    "Add / Select User",
+                    on_click=lambda: ui.open("/users"),
+                    icon="link",
+                ).props("flat").classes("place-items-start")
+                ui.button(
+                    "Add / Select Digital Twins",
+                    on_click=lambda: ui.open("/digital-twins"),
+                    icon="link",
+                ).props("flat").classes("place-items-start")
+                ui.button(
+                    "Build / Run Executions",
+                    on_click=lambda: ui.open("/executions"),
+                    icon="link",
+                ).props("flat").classes("place-items-start")  
+            ui.mermaid(
+                """
+                graph TB;
+                    CA0[Component A]
+                    CB0[Component B]
+                    CC0[Component C]             
+                    subgraph WT[Execution of a Digital Twin]
+                        direction LR
+                        CA0 --> CB0
+                        CB0 --> CC0
+                    end          
+                """
+            ).classes("w-full")            
+        with ui.column().classes("w-full"):
+            ui.markdown(
+                """
+                ### Components
+                """
+            ).classes("w-full") 
+            with ui.grid(columns=1):
+                ui.button(
+                    "Add / View Components",
+                    on_click=lambda: ui.open("/components"),
+                    icon="link",
+                ).props("flat").classes("place-items-start")          
+            ui.mermaid(
+                """
+                graph TB;
+                    CA0[Component A]
+                    CB0[Component B]
+                    CC0[Component C]             
+                """
+            ).classes("w-full")                
