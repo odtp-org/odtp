e4cc70db5c79d488efa94ff83897ef075a468c39
caviri <45425937+caviri@users.noreply.github.com>
2024-06-28 10:47:43 +0200

    chore(test_connection & folder setup): Folder creation for users

--- odtp/helpers/filesystem.py
+++ odtp/helpers/filesystem.py
@@ -5,19 +5,15 @@ import os
 import odtp.helpers.settings as config
 
 def create_main_folders(odtp_path = config.ODTP_PATH):
-    components_folder = os.path.join(odtp_path, 'components')
     users_folder = os.path.join(odtp_path, 'users')
 
-    os.makedirs(components_folder, exist_ok=True)
     os.makedirs(users_folder, exist_ok=True)
 
-    logging.info("Folders created: %s, %s", components_folder, users_folder)
+    logging.info("Folders created: %s, %s", users_folder)
 
-def deleted_main_folders(odtp_path = config.ODTP_PATH):
-    components_folder = os.path.join(odtp_path, 'components')
+def delete_main_folders(odtp_path = config.ODTP_PATH):
     users_folder = os.path.join(odtp_path, 'users')
 
-    shutil.rmtree(components_folder)
     shutil.rmtree(users_folder)
 
-    logging.info("Folders deleted: %s, %s", components_folder, users_folder)
+    logging.info("Folders deleted: %s, %s", users_folder)
