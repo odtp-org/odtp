9b4aeba0997addbf4311259812e0c9d44bb25c54
Sabine Maennel <sabinem@users.noreply.github.com>
2024-07-02 11:08:16 +0200

    chore(cli): cleanup execution commands that are only for the gui (#181)
    rename command for streaming logs
    remove command for showing directory folder
    
    Co-authored-by: sabinem <5292683+sabinem@users.noreply.github.com>

--- odtp/cli/execution.py
+++ odtp/cli/execution.py
@@ -9,7 +9,6 @@ import logging
 import odtp.mongodb.db as db
 import odtp.helpers.parse as odtp_parse
 from odtp.workflow import WorkflowManager
-from directory_tree import display_tree
 import os
 
 app = typer.Typer()
@@ -91,25 +90,9 @@ def run(
     else:
         log.info("SUCCESS: containers for the execution have been run")      
 
-@app.command()
-def output(
-
-        execution_id: str = typer.Option(
-        ..., "--execution-id", help="Specify the ID of the execution"
-    ),
-    project_path: str = typer.Option(
-        ..., "--project-path", help="Specify the path for the execution"
-    ),
-): 
-    try:
-        display_tree(project_path)
-    except Exception as e:
-        print(f"ERROR: Output printing failed: {e}")       
-        raise typer.Abort()
-
 
 @app.command()
-def logs(
+def streamlogs(
     project_path: str = typer.Option(
         ..., "--project-path", help="Specify the path for the execution"
     ),
