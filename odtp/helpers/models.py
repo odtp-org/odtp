4888f94678a25cd4089587f9f0e28e06b122e1ad
sabinem <5292683+sabinem@users.noreply.github.com>
2024-12-10 17:47:07 +0100

    feat: update to new odtp.yaml schema

--- odtp/helpers/models.py
+++ odtp/helpers/models.py
@@ -1,6 +1,28 @@
 from pydantic import BaseModel, Field, validator
 from typing import List, Optional, Union
 
+class Author(BaseModel):
+    name: str
+    orcid: Optional[str] = None
+
+class ComponentRepository(BaseModel):
+    url: str
+    doi: Optional[str] = None
+
+class ToolAuthor(BaseModel):
+    name: str
+    orcid: Optional[str] = None
+
+class ToolRepository(BaseModel):
+    url: str
+    doi: Optional[str] = None
+
+class Tool(BaseModel):
+    tool_name: str = Field(alias="tool-name")
+    tool_authors: Optional[List[ToolAuthor]] = Field(alias="tool-authors", default=None)
+    tool_version: str = Field(alias="tool-version")
+    tool_repository: ToolRepository = Field(alias="tool-repository")
+    tool_license: str = Field(alias="tool-license")
 
 class DataInput(BaseModel):
     name: str
@@ -8,13 +30,11 @@ class DataInput(BaseModel):
     path: str
     description: str
 
-
 class DataOutput(BaseModel):
     description: str
     name: str
     path: str
     type: str
-
 
 class Parameter(BaseModel):
     name: str
@@ -24,45 +44,37 @@ class Parameter(BaseModel):
     options: Optional[List[str]] = None
     allow_custom_value: Optional[bool] = Field(alias="allow-custom-value", default=None)
 
-
 class Port(BaseModel):
     name: str
     description: str
     port_value: int = Field(alias="port-value")
 
-
-class Tool(BaseModel):
-    tool_author: str = Field(alias="tool-author")
-    tool_license: str = Field(alias="tool-license")
-    tool_name: str = Field(alias="tool-name")
-    tool_repository: str = Field(alias="tool-repository")
-    tool_version: str = Field(alias="tool-version")
-
-
-class Devices(BaseModel):
-    gpu: bool
-
+class Device(BaseModel):
+    type: str
+    required: bool
 
 class OdtpDotYamlSchema(BaseModel):
     """Model for odtp.yml parsing"""
-    build_args: Optional[Union[dict, str]] = Field(alias="build-args", default=None)
-    component_author: str = Field(alias="component-author")
-    component_description: str = Field(alias="component-description")
-    component_license: str = Field(alias="component-license")
+    schema_version: str = Field(alias="schema-version")
     component_name: str = Field(alias="component-name")
-    component_repository: str = Field(alias="component-repository")
-    component_type: str = Field(alias="component-type")
     component_version: str = Field(alias="component-version")
-    data_inputs: Optional[List[DataInput]] = Field(alias="data-inputs", default=None)
-    data_output: Optional[List[DataOutput]] = Field(alias="data-output", default=None)
-    devices: Devices
-    parameters: Optional[List[Parameter]] = None
-    ports: Optional[List[Port]] = None
-    schema_input: Optional[Union[dict, str]] = Field(alias="schema-input", default=None)
-    schema_output: Optional[Union[dict, str]] = Field(alias="schema-output", default=None)
-    secrets: Optional[List[Parameter]] = None
+    component_license: str = Field(alias="component-license")
+    component_type: str = Field(alias="component-type")
+    component_description: str = Field(alias="component-description")
+    component_authors: List[Author] = Field(alias="component-authors")
+    component_repository: ComponentRepository = Field(alias="component-repository")
+    component_docker_image: str = Field(alias="component-docker-image")
     tags: List[str]
     tools: Optional[List[Tool]] = None
+    secrets: Optional[Union[List[Parameter], None]] = None
+    build_args: Optional[Union[dict, str]] = Field(alias="build-args", default=None)
+    ports: Optional[List[Port]] = None
+    parameters: Optional[List[Parameter]] = None
+    data_inputs: Optional[List[DataInput]] = Field(alias="data-inputs", default=None)
+    data_output: Optional[List[DataOutput]] = Field(alias="data-output", default=None)
+    schema_input: Optional[Union[dict, str]] = Field(alias="schema-input", default=None)
+    schema_output: Optional[Union[dict, str]] = Field(alias="schema-output", default=None)
+    devices: List[Device]
 
     @validator("component_type")
     def validate_component_type(cls, value):
