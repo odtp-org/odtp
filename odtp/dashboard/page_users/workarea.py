317675270e7efdf0b14fd0bcae6782c86a66aa65
sabinem <5292683+sabinem@users.noreply.github.com>
2025-01-05 17:24:47 +0100

    feat: add secrets setup
    secrets will be stored as encrypted files per user. The files will be added for each docker run where the the step run a component version that has a secret specified in its odtp.yml file

--- odtp/dashboard/page_users/workarea.py
+++ odtp/dashboard/page_users/workarea.py
@@ -1,29 +1,59 @@
-from nicegui import app, ui
+import os
+from nicegui import ui, events
 import odtp.dashboard.utils.ui_theme as ui_theme
+import odtp.helpers.secrets as secrets
 import odtp.dashboard.page_users.storage as storage
+from odtp.helpers.settings import ODTP_PASSWORD
 
 
-def workarea():
-    with ui.row().classes("w-full"):
-        ui.markdown(
-            """
-            ## Manage Users
-            """
-        )
-    current_user = storage.storage_get_current_user()
-    if not current_user:
-        return
-    with ui.row():
-        ui.markdown(
-            f"""
-            #### Current Selection
-            - **user**: {current_user.get("user_name")}
-            - **work directory**: {current_user.get("workdir")}
-            """
-        )
-    with ui.row():
-        ui.button(
-            "Manage digital twins",
-            on_click=lambda: ui.open(ui_theme.PATH_DIGITAL_TWINS),
-            icon="link",
-        )
+class Workarea():
+    def __init__(self):
+        self.current_user = storage.storage_get_current_user()
+        self.ui_workarea()
+
+    def ui_workarea(self):
+        with ui.row().classes("w-full"):
+            ui.markdown(
+                """
+                ## Manage Users
+                """
+            )
+        current_user = storage.storage_get_current_user()
+        if not current_user:
+            return
+        with ui.row():
+            ui.markdown(
+                f"""
+                #### Current Selection
+                - **user**: {current_user.get("user_name")}
+                - **work directory**: {current_user.get("workdir")}
+                """
+            )
+        with ui.column():
+            ui.button(
+                "Manage digital twins",
+                on_click=lambda: ui.open(ui_theme.PATH_DIGITAL_TWINS),
+                icon="link",
+            )
+            ui.upload(
+                on_upload=self.handle_upload,
+                label="upload secrets (store encrypted)"
+            ).classes('max-w-full')
+            ui.button(
+                "get secrets",
+                on_click=self.decrypt_secrets,
+                icon="link",
+            )
+
+    async def handle_upload(self, event):
+        """Handle file upload and encrypt the file."""
+        content = event.content.read().decode('utf-8')
+        file_path = os.path.join(self.current_user["workdir"], f"odtp-secrets.txt")
+
+        salt, iv, encrypted_data = secrets.encrypt_text(content, ODTP_PASSWORD)
+        # Save the encrypted data to a file
+        self.encrypted_file_path = file_path
+        with open(self.encrypted_file_path, "wb") as f:
+            f.write(salt + iv + encrypted_data)
+
+        ui.notify(f"Encrypted file saved to: {self.encrypted_file_path}", type="positive")
