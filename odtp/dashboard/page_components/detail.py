5d4285418c30222520d90bcce3beadd3b51c1fb5
Sabine Maennel <sabinem@users.noreply.github.com>
2025-01-10 17:29:43 +0100

    feat: improve component detail view (#240)
    render all available properties, that seem to be of interest
    
    Co-authored-by: sabinem <5292683+sabinem@users.noreply.github.com>

--- odtp/dashboard/page_components/detail.py
+++ odtp/dashboard/page_components/detail.py
@@ -1,8 +1,8 @@
+from collections import namedtuple
 from nicegui import ui
 import odtp.mongodb.db as db
 
-
-from nicegui import ui
+DisplayItem = namedtuple('DisplayItem', ['key', 'display_value', 'url'])
 
 class VersionDisplay:
     def __init__(self):
@@ -29,15 +29,9 @@ class VersionDisplay:
         """render form elements"""
         self.ui_component_select()
         self.ui_version_select()
+        self.show_component_info()
+        self.show_tools_info()
         self.show_version_info()
-
-    def reset_version_form(self):
-        """reset version form"""
-        self.version = None
-        self.version_id = None
-        self.version_options = None
-        self.ui_version_select.refresh()
-        self.show_version_info.refresh()
 
     @ui.refreshable
     def ui_component_select(self):
@@ -51,8 +45,6 @@ class VersionDisplay:
         ).classes("w-1/2")
 
     def set_component(self, component_id):
-        """called when a new component has been selected"""
-        self.reset_version_form()
         self.compoment_id = component_id
         self.component = db.get_document_by_id(
             document_id=component_id, collection=db.collection_components
@@ -69,6 +61,12 @@ class VersionDisplay:
             for version in versions
         }
         self.ui_version_select.refresh()
+        with ui.grid(columns="1fr 1fr"):
+            with ui.column():
+                self.show_version_info.refresh()
+            with ui.column():
+                self.show_component_info.refresh()
+                self.show_tools_info.refresh()
 
     @ui.refreshable
     def ui_version_select(self):
@@ -89,44 +87,109 @@ class VersionDisplay:
         self.version = db.get_document_by_id(
             document_id=version_id, collection=db.collection_versions
         )
+        self.show_component_info.refresh()
+        self.show_tools_info.refresh()
         self.show_version_info.refresh()
 
     @ui.refreshable
-    def show_version_info(self):
-        """ui element for version info as it comes from github"""
+    def show_component_info(self):
         if not self.version:
             return
-        parameters = self.version.get("parameters", [])
-        if parameters:
-            self.display_dict_list("Parameters", "parameters")
-        ports = self.version.get("ports", [])
-        if ports:
-            self.display_dict_list("Ports", "ports")
-        secrets = self.version.get("secrets", [])
-        if secrets:
-            self.display_dict_list("Secrets", "secrets")
-        self.display_dict_list("Tool Info", "tools")
+        component = self.version["component"]
+        component_display_item_list = [
+            DisplayItem("repository", component["componentName"], component["repoLink"]),
+            DisplayItem("description", self.version["description"], None),
+            DisplayItem("type", component["type"], None),
+            DisplayItem("commit", self.version["commitHash"], None),
+            DisplayItem("build args", self.version["build-args"], None),
+            DisplayItem("created at", self.version["created_at"], None),
+            DisplayItem("license", self.version["license"], None),
+            DisplayItem("image link", component["componentName"], None),
+            DisplayItem("devices", self.version["devices"], None),
+            DisplayItem("odtp_version", self.version["odtp_version"], None),
+        ]
+        ui.label(f"Component Version Info").classes("text-lg")
+        self.display_items_dict_with_links(component_display_item_list)
 
-    def display_dict_list(self, label, dict_list_name):
-        """Display a list of dicts, merging nested structures into key-value pairs."""
-        ui.label(label)
-        with ui.grid(columns='1fr 5fr').classes('w-full gap-0'):
-            for dict_item in self.version.get(dict_list_name, []):
-                # Flatten nested dictionaries or lists
-                for key, value in dict_item.items():
-                    if isinstance(value, dict):  # Merge nested dicts
-                        merged_value = ", ".join(f"{k}: {v}" for k, v in value.items())
-                    elif isinstance(value, list):  # Merge list of dicts or other values
-                        if all(isinstance(i, dict) for i in value):  # List of dicts
-                            merged_value = "; ".join(
-                                ", ".join(f"{k}: {v}" for k, v in sub_item.items())
-                                for sub_item in value
+    @ui.refreshable
+    def show_tools_info(self):
+        if not self.version:
+            return
+        tools = self.version.get("tools")
+        if not tools:
+            self.display_section_empty("Tools Info")
+            return
+        ui.label("Tools Info").classes("text-lg")
+        tool_display_item_list = []
+        for tool in tools:
+            authors = tool.get("tool-authors")
+            if authors:
+                author = authors[0]
+                tool_display_item_list.append(
+                    DisplayItem(
+                        "authors",
+                        author["name"],
+                        author.get("orchid", None)
+                    )
+                )
+                if len(authors) > 1:
+                    for author in authors[1:]:
+                        tool_display_item_list.append(
+                            DisplayItem(
+                                "",
+                                author["name"],
+                                author.get("orchid", None)
                             )
-                        else:  # Simple list
-                            merged_value = ", ".join(str(i) for i in value)
-                    else:  # Simple key-value pair
-                        merged_value = value
+                        )
+            tool_display_item_list.extend([
+                DisplayItem("license", tool.get("tool-license", "no license"), None),
+                DisplayItem("name", tool.get("tool-name"), None),
+                DisplayItem("repository url", tool["tool-repository"].get("url"), None),
+                DisplayItem("           doi", tool["tool-repository"].get("doi"), None),
+                DisplayItem("version", tool.get("tool-version"), None),
+            ])
+            self.display_items_dict_with_links(tool_display_item_list)
 
-                    # Display the key and merged value
-                    ui.label(key).classes('bg-gray-200 border p-1')
-                    ui.label(merged_value).classes('border p-1')
+
+    @ui.refreshable
+    def show_version_info(self):
+        if not self.version:
+            return
+        self.display_section_content("Parameters", "parameters")
+        self.display_section_content("Ports", "ports")
+        self.display_section_content("Secrets", "secrets")
+        self.display_section_content("Inputs", "data-inputs")
+        self.display_section_content("Outputs", "data-outputs")
+
+    def display_section_content(self, label, section_key):
+        section_content = self.version.get(section_key)
+        if not section_content:
+            self.display_section_empty(label)
+        elif isinstance(section_content, dict):
+            ui.label(label).classes("text-lg")
+            self.display_section_dict(section_content)
+        elif isinstance(section_content, list):
+            ui.label(label).classes("text-lg")
+            for list_item in section_content:
+                self.display_section_dict(list_item)
+
+    def display_section_dict(self, section_dict):
+        with ui.grid(columns='1fr 5fr').classes('w-full gap-0'):
+            for key, value in section_dict.items():
+                ui.label(key).classes('bg-gray-200 border p-1')
+                ui.label(value).classes('border p-1')
+
+    def display_section_empty(self, label):
+        with ui.grid(columns='1fr 5fr').classes('w-full gap-0'):
+            ui.label(label).classes('p-1')
+            ui.label("does not apply").classes('p-1')
+
+    def display_items_dict_with_links(self, display_items_list):
+        with ui.grid(columns='1fr 5fr').classes('w-full gap-0'):
+            for item in display_items_list:
+                ui.label(item.key).classes('bg-gray-200 border p-1')
+                if item.url:
+                    ui.link(item.display_value, item.url)
+                else:
+                    ui.label(item.display_value).classes('border p-1')
+
