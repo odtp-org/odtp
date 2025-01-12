aefab161e84c80b0a5f602b75ca8aca2553b4730
sabinem <5292683+sabinem@users.noreply.github.com>
2025-01-12 11:13:29 +0100

    feat: finish execution page
    fix bugs and add improve page flow

--- odtp/dashboard/page_executions/validation.py
+++ odtp/dashboard/page_executions/validation.py
@@ -5,10 +5,7 @@ import odtp.mongodb.db as db
 def validate_execution_name_unique(execution_name, digital_twin_id):
     if len(execution_name) < 6:
         return False
-    print(f"validating {execution_name}")
-    print(f"with {digital_twin_id}")
     executions = db.get_collection(db.collection_executions, query={"digitalTwinRef": ObjectId(digital_twin_id)})
-    print(executions)
     execution_names = {execution.get("title") for execution in executions}
     if execution_name in execution_names:
         return False
